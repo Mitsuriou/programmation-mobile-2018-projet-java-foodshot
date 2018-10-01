4a8bbd7ff98ceba42ac401d0d27220f5dd13cf96
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-10-01 12:25:30 -0400

    Vue de recherche de profil opérationnelle

--- app/src/main/java/ca/qc/cgmatane/informatique/foodshot/ActiviteRechercherProfil.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/foodshot/ActiviteRechercherProfil.java
@@ -2,6 +2,18 @@ package ca.qc.cgmatane.informatique.foodshot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ActiviteRechercherProfil extends AppCompatActivity {
 
@@ -9,5 +21,77 @@ public class ActiviteRechercherProfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activite_rechercher_profil);
+        gererChampRecherche();
+
+        ListView listePseudonymesTrouves = (ListView) findViewById(R.id.liste_pseudonymes_trouves);
+
+        List<HashMap<String, String>> listeLivres = actualiserListe();
+
+        SimpleAdapter adaptateur = new SimpleAdapter(
+                this,
+                listeLivres,
+                android.R.layout.two_line_list_item,
+                new String[] {"nom","pseudonyme"},
+                new int[] {android.R.id.text1, android.R.id.text2}
+            );
+
+        listePseudonymesTrouves.setAdapter(adaptateur);
+
+        listePseudonymesTrouves.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View vue, int positionDansAdapteur, long positionItem) {
+                        ListView vueListeComptes = (ListView)vue.getParent();
+
+                        @SuppressWarnings("unchecked")
+                        HashMap<String,String> compte = (HashMap<String, String>) vueListeComptes.getItemAtPosition((int)positionItem);
+
+                        Toast.makeText(getApplicationContext(),
+                                "Position "+positionItem + " pseudo " + compte.get("pseudonyme"),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
+
+    public void gererChampRecherche() {
+        final EditText champPseudonyme = (EditText) findViewById(R.id.champ_recherche_pseudonyme);
+        champPseudonyme.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        // TODO Faire une requete en BDD
+                        // TODO puis actualiser la liste avec les données récupérées en BDD
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                }
+        );
+    }
+
+    // TEST AFFICHAGE
+    public List<HashMap<String, String>> actualiserListe() {
+        List<HashMap<String,String>> listeComptes = new ArrayList<>();
+
+        HashMap<String,String> compte;
+
+        for (int i = 0; i<11; i++) {
+            compte = new HashMap<>();
+            compte.put("nom", "Dylan Jacquemin"+i);
+            compte.put("pseudonyme", "@Mitsuriou"+i);
+            listeComptes.add(compte);
+        }
+
+        return listeComptes;
+    }
+
+
 }
