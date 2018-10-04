0644b70c371c821105e9aff6e63caab714e9e0c4
Te-nam <pubmjpub@free.fr>
2018-10-04 17:27:33 -0400

    Correction d'un bug de connexion à la BDD et ajout d'une classe permettant de tester la connexion

---
+++ db-connection-test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc-Antoine
+ * Date: 02/10/2018
+ * Time: 09:27
+ */
+
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+
+require_once 'Connection.php';
+
+try {
+    Connection::get()->connect();
+    echo 'Une connection au serveur de base de données PostgreSQL a été établie avec succès.';
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
--- image-upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Marc-Antoine
- * Date: 25/09/2018
- * Time: 08:39
- */
