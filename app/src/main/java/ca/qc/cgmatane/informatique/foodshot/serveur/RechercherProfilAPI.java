08a68ab5b33cf28b715a606d399f969924bf2b58
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-10-11 01:31:23 -0400

    Lecture de Json WIP

--- app/src/main/java/ca/qc/cgmatane/informatique/foodshot/serveur/RechercherProfilAPI.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/foodshot/serveur/RechercherProfilAPI.java
@@ -3,13 +3,14 @@ package ca.qc.cgmatane.informatique.foodshot.serveur;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.json.JSONException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import okhttp3.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class RechercherProfilAPI extends AsyncTask<String, String, String> {
@@ -36,7 +37,31 @@ public class RechercherProfilAPI extends AsyncTask<String, String, String> {
             reponse = client.newCall(request).execute();
             String jsonData = reponse.body().string();
             JSONObject jObject = new JSONObject(jsonData);
-            Log.d("reponse_serveur", jObject.toString());
+            Log.d("json_reponse_serveur", jObject.toString());
+
+            // statut
+            String statut = jObject.getString("statut");
+            Log.d("json_recherche_statut", statut);
+
+            String donneeString = jObject.getString("donnee");
+            JSONObject donneeJson = new JSONObject(donneeString);
+
+            String utilisateurString = donneeJson.getString("utilisateur");
+            JSONArray utilisateurJson = new JSONArray(utilisateurString);
+
+            for (int i = 0; i < utilisateurJson.length(); i++) {
+                Log.d("json_recherche_utilisateur", utilisateurJson.get(i).toString());
+            }
+
+            //message
+            String messageString = jObject.getString("message");
+            JSONArray messageJsonArray = new JSONArray(messageString);
+            JSONObject messageJsonObject;
+            for (int i = 0; i < messageJsonArray.length(); i++) {
+                messageJsonObject = messageJsonArray.getJSONObject(i);
+                Log.d("ALED", messageJsonObject.getString("message"));
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
