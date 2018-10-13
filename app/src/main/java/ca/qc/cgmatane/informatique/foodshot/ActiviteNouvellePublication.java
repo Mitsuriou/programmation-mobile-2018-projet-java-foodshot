a37a272fd6e32be717d6c6885d8deeb1fee2eb5e
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-10-13 15:57:59 -0400

    Correction de bug : changement du mode d'aquisition des coordonnées GPS

--- app/src/main/java/ca/qc/cgmatane/informatique/foodshot/ActiviteNouvellePublication.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/foodshot/ActiviteNouvellePublication.java
@@ -2,18 +2,15 @@ package ca.qc.cgmatane.informatique.foodshot;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.icu.text.SimpleDateFormat;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,9 +20,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -38,6 +38,7 @@ import java.util.Date;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 public class ActiviteNouvellePublication extends AppCompatActivity {
 
@@ -45,12 +46,16 @@ public class ActiviteNouvellePublication extends AppCompatActivity {
     private static final String DOSSIER_PHOTO = "FoodShot";
 
     //localisation
-    private FusedLocationProviderClient client;
+    private final long UPDATE_INTERVAL = 10 * 1000;
+    private final long FASTEST_INTERVAL = 2000;
+    private LocationRequest locationRequest;
     private double latitude;
     private double longitude;
 
+    // enregistrement de la photo prise
     String outputFilePath;
 
+    // composants graphiques
     protected Button boutonCaptureImage;
     protected Button boutonPosterPublication;
 
@@ -60,8 +65,7 @@ public class ActiviteNouvellePublication extends AppCompatActivity {
         setContentView(R.layout.vue_activite_nouvelle_publication);
 
         this.demanderPermissions();
-
-        this.client = LocationServices.getFusedLocationProviderClient(this);
+        startLocationUpdates();
 
         this.boutonCaptureImage = (Button) findViewById(R.id.bouton_demarrer_appareil_photo);
         this.boutonCaptureImage.setOnClickListener(new View.OnClickListener() {
@@ -85,29 +89,8 @@ public class ActiviteNouvellePublication extends AppCompatActivity {
                     finish();
                     return;
                 }
-                if (ActivityCompat.checkSelfPermission(ActiviteNouvellePublication.this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    client.getLastLocation().addOnSuccessListener(ActiviteNouvellePublication.this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                            }
-                            else {
-                                latitude = 0;
-                                longitude = 0;
-                            }
-                        }
-                    });
-                }
-                else {
-                    latitude = 0;
-                    longitude = 0;
-                }
-
                 Log.d("coord_lat", "" + latitude);
                 Log.d("coord_long", "" + longitude);
-
                 Toast.makeText(ActiviteNouvellePublication.this, "Publication postée avec succès ! :D", Toast.LENGTH_SHORT).show();
                 finirActivite();
             }
@@ -192,6 +175,8 @@ public class ActiviteNouvellePublication extends AppCompatActivity {
     private void demanderPermissions() {
         ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_NETWORK_STATE,
                 Manifest.permission.CAMERA,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
         }, 1);
@@ -206,6 +191,40 @@ public class ActiviteNouvellePublication extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    protected void startLocationUpdates() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        if (ActivityCompat.checkSelfPermission(ActiviteNouvellePublication.this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                        @Override
+                        public void onLocationResult(LocationResult locationResult) {
+                            onLocationChanged(locationResult.getLastLocation());
+                        }
+                    },
+                    Looper.myLooper());
+        }
+        else {
+            this.latitude = 0.0;
+            this.longitude = 0.0;
+        }
+
+    }
+
+    public void onLocationChanged(Location location) {
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
     }
 
 }
