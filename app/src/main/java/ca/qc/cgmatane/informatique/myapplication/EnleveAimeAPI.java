f80b6094bbf936b247f91cdc93175ba3f571a110
Lucas PARISOT <lucas.parisot@edu.univ-fcomte.fr>
2018-10-14 00:40:05 -0400

    Ajout de EnleveAimeAPI

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/myapplication/EnleveAimeAPI.java
@@ -0,0 +1,87 @@
+package ca.qc.cgmatane.informatique.myapplication;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class EnleveAimeAPI extends AsyncTask<String, String, String> {
+
+    private int id_utilisateur, id_publication;
+    private boolean statut;
+    private List<ModeleMessage> listeMessages;
+
+    public EnleveAimeAPI(int id_utilisateur, int id_publication) {
+        this.id_utilisateur = id_utilisateur;
+        this.id_publication = id_publication;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        Response reponse;
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://54.37.152.134/api/publication/enleve_aime.php?utilisateur=" + id_utilisateur + "&publication=" + id_publication)
+                .build();
+
+        try {
+            reponse = client.newCall(request).execute();
+            String jsonDonneesString = reponse.body().string();
+            JSONObject jsonDonneesObjet = new JSONObject(jsonDonneesString);
+
+            // statut
+            String statutString = jsonDonneesObjet.getString("statut");
+            this.statut = statutString.equals("true");
+
+            // message
+            String messageString = jsonDonneesObjet.getString("message");
+            JSONArray messageJsonArray = new JSONArray(messageString);
+
+            List<JSONObject> listeDesMessages = new ArrayList<>();
+            for (int i = 0; i < messageJsonArray.length(); i++) {
+                listeDesMessages.add(messageJsonArray.getJSONObject(i));
+            }
+
+            this.listeMessages = new ArrayList<>();
+            for (JSONObject valeur : listeDesMessages) {
+                this.listeMessages.add(new ModeleMessage(
+                        valeur.getInt("code"),
+                        valeur.getString("type"),
+                        valeur.getString("message")
+                ));
+                Log.d("message_code", valeur.getString("code"));
+                Log.d("message_type", valeur.getString("type"));
+                Log.d("message_message", valeur.getString("message"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    public List<ModeleMessage> getListeMessages() {
+        return listeMessages;
+    }
+}
