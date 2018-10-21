e6c77ee5e2971c8d2a0cd31d597ff60a4d21724a
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-10-21 14:58:00 -0400

    Changement de layout pour la vue MonProfil

--- app/src/main/java/ca/qc/cgmatane/informatique/foodshot/ActiviteMonProfil.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/foodshot/ActiviteMonProfil.java
@@ -2,11 +2,7 @@ package ca.qc.cgmatane.informatique.foodshot;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import ca.qc.cgmatane.informatique.foodshot.constantes.Constantes;
 
@@ -15,23 +11,13 @@ public class ActiviteMonProfil extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (getSharedPreferences(Constantes.PREFERENCES_THEME_COULEUR, Context.MODE_PRIVATE).getInt("theme", 1) == 1) {
-            setTheme(R.style.AppThemeNoActionBar);
+            setTheme(R.style.AppTheme);
         }
         else {
-            setTheme(R.style.AppThemeNoirNoActionBar);
+            setTheme(R.style.AppThemeNoir);
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_activite_mon_profil);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Modifier sa photo de profil", Snackbar.LENGTH_LONG)
-                        .setAction("Suivre", null).show();
-            }
-        });
     }
 }
