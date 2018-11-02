7e7a0361dfa85fdc492af42c45b60e0131cec1c0
Lucas PARISOT <lucas-parisot@orange.fr>
2018-11-01 21:55:24 -0400

    Reussi. Les commits de Lucas PARISOT sont de LucasPARISOT

--- app/src/main/java/ca/qc/cgmatane/informatique/foodshot/constantes/Constantes.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/foodshot/constantes/Constantes.java
@@ -5,6 +5,6 @@ public class Constantes {
     public static final String PREFERENCES_GENERALES = "PreferencesGenerales";
     public static final String PREFERENCES_THEME_COULEUR = "PreferencesThemeCouleur";
     public static final String PREFERENCES_LOCALISATION = "PreferencesLocalisation";
-
+ 
 }
  
